9cc3d057f5a8070e7e05b3871a1c1906d6fd7a80
Eujine <sponsor85@mail.ru>
2017-12-09 01:19:31 +0200

    [*] prepare view

---
+++ Razor/Razor/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Razor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        Product myProduct = new Product { ProductID = 1, Name = "CPU" 
+            , Description = "Intel CPU", Category = "Lenova", Price = 30};
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(myProduct);
+        }
+    }
+}
